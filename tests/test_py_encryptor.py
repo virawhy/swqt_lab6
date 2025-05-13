1fe91131990f20a59c5e23af7e6c5dd61df9fc28
virawhy <6334980@stud.nau.edu.ua>
2025-05-13 21:56:59 +0300

    change.yml

--- tests/test_py_encryptor.py
+++ tests/test_py_encryptor.py
@@ -3,7 +3,7 @@ import os
 import pytest
 import base64
 
-from ..src.setup import TextEncryptor  # Use relative import
+from src.setup import TextEncryptor  # Use relative import
 def extract_salt(token):
     decoded = base64.urlsafe_b64decode(token.encode())
     return decoded[:16]
